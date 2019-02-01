b9a4d6ac72e794b85a8c36b5493cd557dd83f875
mob <zx14993@163.com>
2019-02-01 19:48:29 +0800

    First commit

---
+++ AFNetworkingProjectTests/AFNetworkingProjectTests.m
@@ -0,0 +1,37 @@
+//
+//  AFNetworkingProjectTests.m
+//  AFNetworkingProjectTests
+//
+//  Created by mob on 2019/1/29.
+//  Copyright © 2019年 mob. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNetworkingProjectTests : XCTestCase
+
+@end
+
+@implementation AFNetworkingProjectTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
